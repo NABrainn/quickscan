add4472f5be1ff928d842c17556c020a2d6444b5
NAbrain <michal2002.60@gmail.com>
2025-01-21 11:07:43 +0100

    update service

--- src/app/scanner/views/scanner/service/entry-service.ts
+++ src/app/scanner/views/scanner/service/entry-service.ts
@@ -1,25 +1,20 @@
 import { Injectable } from "@angular/core";
-import { Invoice, Product, Vendor } from "../../../dto/Invoice";
-import { EntrySet } from "../../../dto/EntrySet";
+import { InvoiceEntry } from "../../../dto/InvoiceEntry";
 import { InvoiceStore } from "../data/invoice-store";
 
 
 @Injectable({providedIn: 'root'})
-export class EntryService {
+export class InvoiceService {
 
     constructor(private store: InvoiceStore){}
 
     invoiceToEntries() {
         const invoice = this.store.getInvoice();
-        const entries: EntrySet[] = [];
+        const entries: InvoiceEntry[] = [];
 
         Object.entries(invoice).forEach(([k, v]) => {
-        entries.push({attribute: k, value: v, open: false} as EntrySet);
+        entries.push({attribute: k, value: v} as InvoiceEntry);
         })
         return entries;
-    }
-
-    instanceofObject(param: any): boolean {
-        return param instanceof Object;
     }
 }
