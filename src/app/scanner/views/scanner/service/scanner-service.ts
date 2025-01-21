a986fb938bc2840861f1f7226c1443c666417c6b
NAbrain <michal2002.60@gmail.com>
2025-01-21 11:02:49 +0100

    add service to for cross component communication inside scanner component

---
+++ src/app/scanner/views/scanner/service/scanner-service.ts
@@ -0,0 +1,25 @@
+import { computed, Injectable, signal } from "@angular/core";
+import { InvoiceEntry } from "../../../dto/InvoiceEntry";
+import { InvoiceService } from "./entry-service";
+
+interface InvoiceEntryWithOpen extends InvoiceEntry {
+  open: boolean;
+}
+
+@Injectable({providedIn: 'root'})
+export class scannerService {
+
+    constructor(private invoiceService: InvoiceService){}
+
+    private _entries = signal<InvoiceEntryWithOpen[]>([]);
+    computedEntries = computed(() => this._entries());
+
+    initEntries() {
+        const entries = this.invoiceService.invoiceToEntries();
+        this._entries.set(entries.map(entry => ({...entry, open: false})))
+    }
+
+    openItem(next: InvoiceEntry) {
+        this._entries.update(curr => curr.map(item => item.attribute === next.attribute && next.value instanceof Object ? {...item, open: !item.open} : item))    
+    }
+}
