c18ab02b8f11fa623b08d3beca373721c9ef3631
NAbrain <michal2002.60@gmail.com>
2025-01-21 11:06:22 +0100

    extract signal logic to a service

--- src/app/scanner/views/scanner/components/list/list.component.ts
+++ src/app/scanner/views/scanner/components/list/list.component.ts
@@ -1,28 +1,43 @@
-import { Component, computed, signal, WritableSignal } from '@angular/core';
+import { Component, computed, model, signal, WritableSignal } from '@angular/core';
 
 import { Invoice } from '../../../../dto/Invoice';
-import { EntryService } from '../../service/entry-service';
-import { KeyValuePipe, NgClass } from '@angular/common';
-import { EntrySet } from '../../../../dto/EntrySet';
+import { InvoiceService } from '../../service/entry-service';
+import { JsonPipe, KeyValuePipe, NgClass } from '@angular/common';
+import { InvoiceEntry } from '../../../../dto/InvoiceEntry';
+import { SeparatorPipe } from '../../pipes/entry-pipe';
+import { TypeCheckService } from '../../../../shared/type-check-service';
+import { scannerService } from '../../service/scanner-service';
+
+interface InvoiceEntryWithOpen extends InvoiceEntry {
+  open: boolean;
+}
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [KeyValuePipe, NgClass],
+  imports: [SeparatorPipe, JsonPipe, NgClass],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  constructor(public entryService: EntryService){}
+  constructor(private invoiceService: InvoiceService, public tcService: TypeCheckService, private scannerService: scannerService){}
 
-  private _entries = signal<EntrySet[]>([]);
-  computedEntries = computed(() => this._entries());
+  openItem(next: InvoiceEntry) {
+    this.scannerService.openItem(next)
+  }
 
-  openItem(input: EntrySet) {
-    this._entries.update(curr => curr.map(item => item.attribute === input.attribute && input.value instanceof Object ? {...item, open: !item.open} : item))
+  getComputedEntries() {
+    return this.scannerService.computedEntries();
+  }
+
+  getEntryValues(entry: InvoiceEntry) {
+    if(Array.isArray(entry.value)){    
+      return entry.value.map(o => Object.entries(o));
+    }
+    return Object.entries({...entry.value})
   }
 
   ngOnInit() {
-    this._entries.set(this.entryService.invoiceToEntries())
+
   }
 }
