5bdd71820423050346cd6b55c0811f6cecaa28c2
NAbrain <michal2002.60@gmail.com>
2025-01-25 09:11:53 +0100

    remove unnecessary logic

--- src/app/scanner/views/scanner/service/scanner-service.ts
+++ src/app/scanner/views/scanner/service/scanner-service.ts
@@ -1,29 +1,21 @@
 import { computed, Injectable, signal } from "@angular/core";
-import { InvoiceEntry } from "../../../dto/InvoiceEntry";
+import { ListItem } from "../../../dto/ListItem";
 import { InvoiceService } from "./entry-service";
 
-interface InvoiceEntryWithOpen extends InvoiceEntry {
-  open: boolean;
-}
-
 @Injectable({providedIn: 'root'})
-export class scannerService {
+export class ScannerService {
 
     constructor(private invoiceService: InvoiceService){}
 
-    private _entries = signal<InvoiceEntryWithOpen[]>([]);
-    computedEntries = computed(() => this._entries());
+    private _items = signal<ListItem[]>([]);
+    items = computed(() => this._items());
 
     private _showAll = signal(false);
     showAll = computed(() => this._showAll())
 
     initEntries() {
-        const entries = this.invoiceService.invoiceToEntries();
-        this._entries.set(entries.map(entry => ({...entry, open: false})))
-    }
-
-    openItem(next: InvoiceEntry) {
-        this._entries.update(curr => curr.map(item => item.attribute === next.attribute && next.value instanceof Object ? {...item, open: !item.open} : item))    
+        const items = this.invoiceService.invoiceToEntries();
+        this._items.set(items);
     }
 
     toggleListContent() {
