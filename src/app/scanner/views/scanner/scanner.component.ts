97170ab882822d9ba5369dd710838226bb8b3cec
NAbrain <michal2002.60@gmail.com>
2025-01-27 19:37:21 +0100

    add multi-step component that wraps upload and list components

--- src/app/scanner/views/scanner/scanner.component.ts
+++ src/app/scanner/views/scanner/scanner.component.ts
@@ -1,25 +1,46 @@
-import { Component, computed, effect, inject, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { ListComponent } from './components/list/list.component';
-import { InvoiceService } from './service/entry-service';
 import { ScannerService } from './service/scanner-service';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
+import { UploadPanelComponent } from './components/upload-panel/upload-panel.component';
+import { NgClass } from '@angular/common';
+import { MultiStepFormComponent } from './components/multi-step-form/multi-step-form.component';
+
+export type Tab = {
+  name?: string,
+  selected?: boolean,
+  enabled?: boolean   
+}
 
 @Component({
   selector: 'app-scanner',
   standalone: true,
-  imports: [ListComponent],
+  imports: [ListComponent, ListItemComponent, ButtonComponent, UploadPanelComponent, NgClass, MultiStepFormComponent],
   templateUrl: './scanner.component.html',
   styleUrl: './scanner.component.css'
 })
 export class ScannerComponent {
 
-  constructor(private invoiceService: InvoiceService, private scannerService: ScannerService){}
+  _tabs = signal<Tab[]>([
+    {name: 'Upload', selected: true, enabled: true},
+    {name: 'Ready', selected: false, enabled: true}
+  ]);
+  tabs = computed(() => this._tabs())
+
+
+
+  constructor(private scannerService: ScannerService){
+  }
 
   getItems() {
     return this.scannerService.items();
-  }
-  
-  ngOnInit(): void {
-    this.scannerService.initEntries()
+    // return [];
   }
 
+
+  
+  ngOnInit(): void {
+    this.scannerService.initEntries();
+  }
 }
