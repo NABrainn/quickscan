a91224202687d1a8f9c6f426e6635a6e1d902508
NAbrain <michal2002.60@gmail.com>
2025-01-27 10:16:31 +0100

    add Items prop to list component, chop off redundant functions

--- src/app/scanner/views/scanner/components/list/list.component.ts
+++ src/app/scanner/views/scanner/components/list/list.component.ts
@@ -1,37 +1,17 @@
 import { Component, computed, model, signal, WritableSignal } from '@angular/core';
 
-import { Invoice } from '../../../../dto/Invoice';
-import { InvoiceService } from '../../service/entry-service';
-import { NgClass, SlicePipe } from '@angular/common';
-import { ListItem } from '../../../../dto/ListItem';
-import { TypeCheckService } from '../../../../shared/type-check-service';
-import { ScannerService } from '../../service/scanner-service';
 import { ListItemComponent } from '../list-item/list-item.component';
-import { CamelCaseToTextPipe } from '../../pipes/camel-case-to-text-pipe';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [SlicePipe, ListItemComponent,CamelCaseToTextPipe],
+  imports: [ListItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  constructor(private invoiceService: InvoiceService, public tcService: TypeCheckService, private scannerService: ScannerService){}
 
-  getItems() {
-    return this.scannerService.items();
-  }
+  items = model<any[]>();
 
-  getShowAll() {
-    return this.scannerService.showAll();
-  }
-
-  toggleListContent() {
-    this.scannerService.toggleListContent();
-  }
-
-  ngOnInit() {
-
-  }
+  constructor(){}
 }
