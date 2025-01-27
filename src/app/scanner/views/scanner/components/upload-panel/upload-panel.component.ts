89682334662321c64689177801fbd70fd4f5ad72
NAbrain <michal2002.60@gmail.com>
2025-01-27 19:37:40 +0100

    add upload component

---
+++ src/app/scanner/views/scanner/components/upload-panel/upload-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-panel.component.html',
+  styleUrl: './upload-panel.component.css'
+})
+export class UploadPanelComponent {
+
+}
