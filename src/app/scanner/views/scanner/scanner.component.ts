bd4a9db0a039981357d150f25753f63bde81afc8
NAbrain <michal2002.60@gmail.com>
2025-01-29 17:33:21 +0100

    update multi-step form to make the code more modular

--- src/app/scanner/views/scanner/scanner.component.ts
+++ src/app/scanner/views/scanner/scanner.component.ts
@@ -1,35 +1,22 @@
-import { Component, computed, effect, signal } from '@angular/core';
-import { ListComponent } from './components/list/list.component';
+import { Component } from '@angular/core';
+import { ListComponent } from '../../shared/components/list/list.component';
 import { ScannerService } from './service/scanner-service';
 import { ButtonComponent } from '../../shared/components/button/button.component';
-import { ListItemComponent } from './components/list-item/list-item.component';
-import { UploadPanelComponent } from './components/upload-panel/upload-panel.component';
+import { ListItemComponent } from '../../shared/components/list-item/list-item.component';
+import { UploadPanelComponent } from '../../shared/components/upload-panel/upload-panel.component';
 import { NgClass } from '@angular/common';
-import { MultiStepFormComponent } from './components/multi-step-form/multi-step-form.component';
-
-export type Tab = {
-  name?: string,
-  selected?: boolean,
-  enabled?: boolean   
-}
+import { MultiStepFormComponent } from '../../shared/components/multi-step-form/multi-step-form.component';
+import { TabComponent } from '../../shared/components/tab/tab.component';
 
 @Component({
   selector: 'app-scanner',
   standalone: true,
-  imports: [ListComponent, ListItemComponent, ButtonComponent, UploadPanelComponent, NgClass, MultiStepFormComponent],
+  imports: [ListComponent, ListItemComponent, ButtonComponent, UploadPanelComponent, NgClass, MultiStepFormComponent, TabComponent],
   templateUrl: './scanner.component.html',
   styleUrl: './scanner.component.css'
 })
 export class ScannerComponent {
-
-  _tabs = signal<Tab[]>([
-    {name: 'Upload', selected: true, enabled: true},
-    {name: 'Ready', selected: false, enabled: true}
-  ]);
-  tabs = computed(() => this._tabs())
-
-
-
+  
   constructor(private scannerService: ScannerService){
   }
 
@@ -37,10 +24,9 @@ export class ScannerComponent {
     return this.scannerService.items();
     // return [];
   }
-
-
   
   ngOnInit(): void {
     this.scannerService.initEntries();
+
   }
 }
