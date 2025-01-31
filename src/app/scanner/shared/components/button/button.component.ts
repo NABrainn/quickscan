37fa126030e1e2bed591c2a02fd8ebec00d1ddaa
NAbrain <michal2002.60@gmail.com>
2025-01-31 14:35:05 +0100

    add property for updating color

--- src/app/scanner/shared/components/button/button.component.ts
+++ src/app/scanner/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, input, Renderer2, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+
+  btn = viewChild('button', {'read': ElementRef});
+  bgc = input<'one' | 'two' | 'three' | 'four' | 'neutral'>('four');
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.renderer.setStyle(this.btn()?.nativeElement, 'background-color', `var(--${this.bgc()})`)
+  }
   
 }
