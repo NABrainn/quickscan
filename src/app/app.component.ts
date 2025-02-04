80e92e06eaeec40287f2d4e86eb32fd2f9197c5b
NAbrain <michal2002.60@gmail.com>
2025-02-04 12:59:03 +0100

    import loading bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ScannerComponent } from './scanner/views/scanner/scanner.component';
+import { LoadingBarComponent } from '@components/loading-spinner/loading-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ScannerComponent],
+  imports: [RouterOutlet, ScannerComponent, LoadingBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'scannerIO';
+  
 }
