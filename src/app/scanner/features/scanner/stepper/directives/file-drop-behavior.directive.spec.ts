f85db16d36c2e722045c97df132d6e515fbcdb30
NAbrain <michal2002.60@gmail.com>
2025-02-05 18:24:03 +0100

    delete

--- src/app/scanner/features/scanner/stepper/directives/file-drop-behavior.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FileDropBehaviorDirective } from "./file-drop-behavior.directive";
-
-describe('FileUploadNextStepDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FileDropBehaviorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/scanner/features/scanner/stepper/directives/file-select-behavior.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FileSelectBehaviorDirective } from './file-select-behavior.directive';
-
-describe('FileSelectBehaviorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FileSelectBehaviorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
