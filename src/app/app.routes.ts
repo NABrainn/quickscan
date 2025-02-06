f970abae29d8c58f2344736b09ab7c64d455ee82
NAbrain <michal2002.60@gmail.com>
2025-02-06 10:50:44 +0100

    setup router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { ScannerComponent } from './scanner/features/scanner/scanner.component';
+import { ListComponent } from './scanner/ui/list/list.component';
+import { UploadPanelComponent } from './scanner/ui/upload-panel/upload-panel.component';
+import { PageNotFoundComponent } from './scanner/core/components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'scanner', 
+        component: ScannerComponent,
+        children: [
+            { path: 'upload', component: UploadPanelComponent },
+            { path: 'ready', component: ListComponent }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/scanner',
+        pathMatch: 'full'
+    },
+    
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+
+];
