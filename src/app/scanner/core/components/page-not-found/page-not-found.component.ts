a157b52d01f2d5d63942f0e8951857ea5b01149f
NAbrain <michal2002.60@gmail.com>
2025-02-06 10:50:36 +0100

    add page not found

---
+++ src/app/scanner/core/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TextColorDirective } from 'app/scanner/shared/directives/text-color/text-color.directive';
+import { CardComponent } from 'app/scanner/ui/card/card.component';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [
+    CardComponent,
+    RouterLink,
+    TextColorDirective
+  ],
+  templateUrl: './page-not-found.component.html',
+  styleUrl: './page-not-found.component.css'
+})
+export class PageNotFoundComponent {
+
+}
