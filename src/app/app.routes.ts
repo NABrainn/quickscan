1baf3e1aaac24d2bd03536d5cac58750cc3d666b
NAbrain <michal2002.60@gmail.com>
2025-02-08 17:44:18 +0100

    add scanner component and a route to it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,19 @@
 import { Routes } from '@angular/router';
+import { ScannerComponent } from './scanner/features/scanner/scanner.component';
 
 export const routes: Routes = [
+    { 
+        path: 'scanner', component: ScannerComponent
+    },
+    {
+        path: '',
+        redirectTo: 'scanner',
+        pathMatch: 'full'
+    },
+    
+    // {
+    //     path: '**',
+    //     component: PageNotFoundComponent
+    // }
 
 ];
---
+++ src/app/scanner/features/scanner/scanner.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatStepperModule} from '@angular/material/stepper'; 
+
+@Component({
+  selector: 'app-scanner',
+  imports: [
+    MatStepperModule
+  ],
+  templateUrl: './scanner.component.html',
+  styleUrl: './scanner.component.css'
+})
+export class ScannerComponent {
+
+}
