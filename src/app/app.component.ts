6630e047afaf114d75bbab13867c1cd90e2f727f
NAbrain <michal2002.60@gmail.com>
2025-02-08 16:01:27 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoadingBarComponent } from 'app/scanner/ui/loading-bar/loading-bar.component';
-import { ScannerComponent } from './scanner/features/scanner/scanner.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ScannerComponent, LoadingBarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
