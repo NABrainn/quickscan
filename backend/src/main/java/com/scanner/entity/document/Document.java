40aeea493b1990ef8111cf6a180d4b67c36ad9a4
NAbrain <michal2002.60@gmail.com>
2025-02-09 15:15:50 +0100

    add @Getter and @Setter

--- backend/src/main/java/com/scanner/entity/document/Document.java
+++ backend/src/main/java/com/scanner/entity/document/Document.java
@@ -1,7 +1,11 @@
 package com.scanner.entity.document;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Document {
