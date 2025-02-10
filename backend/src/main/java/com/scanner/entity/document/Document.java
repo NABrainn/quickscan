f4a20b8cdb0866cbf13f98f4ca0470d248a1ae31
NAbrain <michal2002.60@gmail.com>
2025-02-10 19:29:01 +0100

    update to use Builder annotation add JsonSubType annotations pointing at Receipt and Invoice

--- backend/src/main/java/com/scanner/entity/document/Document.java
+++ backend/src/main/java/com/scanner/entity/document/Document.java
@@ -1,13 +1,27 @@
 package com.scanner.entity.document;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+@NoArgsConstructor
+@SuperBuilder
 @Getter
 @Setter
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        property = "documentType"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Receipt.class, name = "receipt"),
+        @JsonSubTypes.Type(value = Invoice.class, name = "invoice")
+})
 public abstract class Document {
 
     @Id
