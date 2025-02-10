9207072565a2ab353f88e3334bf0975f6658ab20
NAbrain <michal2002.60@gmail.com>
2025-02-10 19:30:09 +0100

    add document controller

---
+++ backend/src/main/java/com/scanner/controller/DocumentController.java
@@ -0,0 +1,43 @@
+package com.scanner.controller;
+
+import com.scanner.entity.document.Document;
+import com.scanner.service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/documents")
+public class DocumentController {
+
+    private DocumentService documentService;
+
+    @Autowired
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    @PostMapping
+    public Document saveDocument(@RequestBody Document document) {
+        return documentService.saveDocument(document);
+    }
+
+    @GetMapping("/{id}")
+    public Document getDocumentById(@PathVariable long id) {
+        return documentService.getDocumentById(id);
+    }
+
+    @GetMapping
+    public List<Document> getAllDocuments() {
+        return documentService.getAllDocuments();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteDocument(@RequestParam long id) {
+        return new ResponseEntity<>("Document with id " + id + " deleted.", HttpStatus.OK);
+    }
+}
