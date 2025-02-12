1e64c1c771db92afcbb43c590f877aee33a39315
NAbrain <michal2002.60@gmail.com>
2025-02-12 12:51:04 +0100

    chat client now determines the system message based on jpg content

--- backend/src/main/java/com/scanner/configuration/ChatClientConfiguration.java
+++ backend/src/main/java/com/scanner/configuration/ChatClientConfiguration.java
@@ -9,19 +9,66 @@ public class ChatClientConfiguration {
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         return builder.defaultSystem("""
-                        Twoim zadaniem jest skanowanie paragonów. Każdy paragon zamieniaj na JSON w podanym formacie:
+                        Sprawdź jaki rodzaj dokumentu został przesłany: paragon lub faktura, a następnie zrealizuj poniższe zadania:
+                        
+                        przeczytaj zdjęcie faktury i uzyskaj z niego nastepujące informacje. 
+                        Całość napisz w podanym formacie json zmieniając tylko value, key muszą zostać nie zmienione. 
+                        Napisz tylko json w podanym niżej formacie. 
+                        Jeśli jest informacja w nawiasie, zastosuj się do tej informacji, nie wstawiając jej w odpowiedź. 
+                        Jeśli nie udało się znaleźć informacji, napisz 'null'. 
+                        Dane MUSZĄ mieć następujące nazwy:
+                        {
+                        "type": "invoice"
+                        "odbiorca":{
+                        "nazwa":"nazwa",
+                        "nip":"nip",
+                        "adres":"adres"
+                        },
+                        "sprzedawca":{ 
+                        "nazwa":"nazwa", 
+                        "nip":"nip", 
+                        "adres":"adres"
+                        },
+                        "numerFaktury": "numerFaktury",  
+                        "nrRachunkuBankowego": "nrRachunkuBankowego", 
+                        "dataWystawienia": "dataWystawienia",  
+                        "dataSprzedazy": "dataSprzedazy",  
+                        "razemNetto": "razemNetto",  
+                        "razemStawka": "razemStawka",  
+                        "razemPodatek": "razemPodatek",  
+                        "razemBrutto": "razemBrutto", 
+                        "waluta": "waluta",  
+                        "formaPlatnosci": "formaPlatnosci",   
+                        "produkty": [
+                            {     
+                            "nazwaProduktu": "nazwaProduktu",    
+                            "jednostkaMiary": "jednostkaMiary" (zobacz czy nie istnieje skrót 'j. m.' zapisz wartość jako value jednostkiMiary. key jednostkaMiary bez zmian),      
+                            "ilosc":  "ilosc"(jeśli ilość nie jest integerem napisz tylko float np. 0.55, zawsze w String),  
+                            "wartoscNetto": "wartoscNetto",  
+                            "stawkaVat": "stawkaVat",  
+                            "podatekVat": "podatekVat",  
+                            "brutto": "brutto" 
+                            }  
+                            ]
+                        }
+
+                        przeczytaj zdjęcie paragonu i uzyskaj z niego nastepujące informacje. 
+                        Całość napisz w podanym formacie json zmieniając tylko value, key muszą zostać nie zmienione. 
+                        Napisz tylko json w podanym niżej formacie. Jeśli jest informacja w nawiasie, zastosuj się do tej informacji, nie wstawiając jej w odpowiedź. 
+                        Jeśli nie udało się znaleźć informacji, napisz 'null'. Dane MUSZĄ mieć następujące nazwy:
                         { 
+                        "type": "receipt",
                         "dataZakupu": "dataZakupu",  
                         "nazwaSklepu": "nazwaSklepu", 
                         "kwotaCalkowita": "kwotaCalkowita",  
                         "produkty": [    
                             {     
-                                "nazwaProduktu": "nazwaProduktu",    
-                                "cenaSuma": "cenaJednostkowa",      
-                                "ilosc": "ilosc" (jeśli ilość nie jest integerem napisz tylko float np. 0.55)    
+                            "nazwaProduktu": "nazwaProduktu",    
+                            "cenaSuma": "cenaSuma",      
+                            "ilosc":  "ilosc"(jeśli ilość nie jest integerem napisz tylko float np. 0.55)    
                             }  
                         ]
-                    }
+                        }
                         """)
                 .build();
     }
