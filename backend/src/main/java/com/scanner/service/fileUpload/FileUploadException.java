f13b11406c772cf5e7b910b53341e7d4a733924a
NAbrain <michal2002.60@gmail.com>
2025-02-12 12:54:16 +0100

    add exception type for FileUploadService

---
+++ backend/src/main/java/com/scanner/service/fileUpload/FileUploadException.java
@@ -0,0 +1,7 @@
+package com.scanner.service.fileUpload;
+
+public class FileUploadException extends RuntimeException{
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
