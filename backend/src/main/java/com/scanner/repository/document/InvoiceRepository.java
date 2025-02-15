ba3666f0d98645a15a5c23e2bac4f5bba3eed938
NAbrain <michal2002.60@gmail.com>
2025-02-15 16:46:50 +0100

    add custom query to find by owner

--- backend/src/main/java/com/scanner/repository/document/InvoiceRepository.java
+++ backend/src/main/java/com/scanner/repository/document/InvoiceRepository.java
@@ -1,7 +1,11 @@
 package com.scanner.repository.document;
 
+import com.scanner.entity.document.Document;
 import com.scanner.entity.document.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
+    List<Invoice> findByCreatedBy(String createdBy);
 }
