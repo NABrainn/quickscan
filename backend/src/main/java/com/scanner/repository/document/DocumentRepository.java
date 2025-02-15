874d2b2d5255aefcb3b3953685400d4f61988985
NAbrain <michal2002.60@gmail.com>
2025-02-15 16:56:28 +0100

    remove unused imports

--- backend/src/main/java/com/scanner/repository/document/DocumentRepository.java
+++ backend/src/main/java/com/scanner/repository/document/DocumentRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface DocumentRepository extends JpaRepository<Document, Long> {
     Page<Document> findByCreatedBy(String createdBy, Pageable pageable);
 }
