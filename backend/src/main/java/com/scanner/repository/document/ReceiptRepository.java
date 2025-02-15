396005e8647c8e3f62e7c8aa7d41b49a7969a473
NAbrain <michal2002.60@gmail.com>
2025-02-15 16:48:15 +0100

    update repo

--- backend/src/main/java/com/scanner/repository/document/ReceiptRepository.java
+++ backend/src/main/java/com/scanner/repository/document/ReceiptRepository.java
@@ -1,9 +1,13 @@
 package com.scanner.repository.document;
 
+import com.scanner.entity.document.Document;
 import com.scanner.entity.document.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+    List<Receipt> findByCreatedBy(String createdBy);
 }
