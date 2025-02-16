3906e5a74eefa631d1a1ca7d0aaaf42a96a9f223
NAbrain <michal2002.60@gmail.com>
2025-02-16 18:57:08 +0100

    add attaching tokens to requests after keycloak auth

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import { AutoRefreshTokenService, KeycloakService, provideKeycloak, UserActivityService, withAutoRefreshToken } from 'keycloak-angular';
+import { AutoRefreshTokenService, INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG, includeBearerTokenInterceptor, KeycloakService, provideKeycloak, UserActivityService, withAutoRefreshToken } from 'keycloak-angular';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -13,7 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(
-      withInterceptors([LoadingInterceptor])
+      withInterceptors([LoadingInterceptor, includeBearerTokenInterceptor])
     ),
     provideKeycloak({
       config: {
@@ -27,6 +27,12 @@ export const appConfig: ApplicationConfig = {
       },
       
     }),
-
+    {
+      provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+      useValue: [{
+        urlPattern: /^http:\/\/localhost:8080\/api\//,
+        httpMethods: ['GET', 'POST', 'PUT', 'DELETE']
+      }]
+    }
   ]
 };
