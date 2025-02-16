68809135884fcc6a950771a5f615552fe0b8a8b0
NAbrain <michal2002.60@gmail.com>
2025-02-16 18:55:30 +0100

    handle wildcard routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,22 +1,23 @@
 import { Routes } from '@angular/router';
 import { ScannerComponent } from './scanner/features/scanner/scanner.component';
 import { canActivateAuthRole } from './scanner/core/auth/auth-guard';
+import { PageNotFoundComponent } from './scanner/core/auth/components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { 
         path: 'scanner', component: ScannerComponent,
         canActivate: [canActivateAuthRole]
     },
-    // {
-    //     path: '',
-    //     redirectTo: 'scanner',
-    //     pathMatch: 'full',
+    {
+        path: '',
+        redirectTo: 'scanner',
+        pathMatch: 'full',
         
-    // },
+    },
     
-    // {
-    //     path: '**',
-    //     component: PageNotFoundComponent
-    // }
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
 
 ];
