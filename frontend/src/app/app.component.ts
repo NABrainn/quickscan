21fad108e0ced9c2e2602cca215d5ba174d6a5b1
NAbrain <michal2002.60@gmail.com>
2025-02-17 19:53:10 +0100

    inject LoadingService to manage loading bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatProgressBar, MatProgressBarModule} from '@angular/material/progress-bar'; 
+import { LoadingService } from './scanner/core/loading.service';
 
 @Component({
     selector: 'app-root',
     imports: [
-        RouterOutlet
+        RouterOutlet,
+        MatProgressBar
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+    loadingService = inject(LoadingService);
+
+    isLoading() {
+        return this.loadingService.isLoading();
+    }
 }
