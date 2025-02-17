f9ce3037fce07069810743ab845b779e7cc552c5
NAbrain <michal2002.60@gmail.com>
2025-02-17 19:55:30 +0100

    add file for types

---
+++ frontend/src/app/scanner/shared/types.ts
@@ -0,0 +1,55 @@
+export type Invoice = {
+    type?: 'invoice',
+    numerFaktury?: string,
+    nrRachunkuBankowego?: string,
+    dataWystawienia?: string,
+    dataSprzedaży?: string,
+    razemNetto?: number,
+    razemRata?: number,
+    razemPodatek?: string,
+    razemBrutto?: number,
+    waluta?: string,
+    formaPłatności?: string,
+    odbiorca?: Client,
+    sprzedawca?: Vendor,
+    produkty?: InvoiceProduct[]
+}
+
+export type Receipt = {
+    type?: 'receipt',
+    dataZakupu?: string,
+    nazwaSklepu?: string,
+    kwotaCałkowita?: number,
+    produkty?: ReceiptProduct[]
+}
+
+export type InvoiceProduct = {
+    nazwaProduktu?: string,
+    ilość?: number,
+    cenaSuma?: number,
+    jednostkaMiary?: string,
+    wartośćNetto?: number,
+    stawkaVAT?: number,
+    podatekVAT?: string,
+    wartośćBrutto?: number,
+
+}
+
+export type ReceiptProduct = {
+    nazwaProduktu?: string,
+    ilość?: number,
+    cenaSuma?: number
+}
+
+export type Client = {
+    nazwa?: string,
+    nip?: string,
+    adres?: string
+}
+
+export type Vendor = {
+    nazwa?: string,
+    nip?: string,
+    adres?: string
+}
+
