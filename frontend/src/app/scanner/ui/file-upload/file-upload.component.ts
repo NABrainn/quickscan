f4728c181d3dd0fe569dc22d98ceceb130519547
NAbrain <michal2002.60@gmail.com>
2025-02-17 13:00:36 +0100

    update fileupload component: -receives validation inputs -outputs form data

--- frontend/src/app/scanner/ui/file-upload/file-upload.component.ts
+++ frontend/src/app/scanner/ui/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input, model, OnInit, signal } from '@angular/core';
+import { Component, computed, Input, input, model, OnInit, output, signal } from '@angular/core';
 import {MatCardModule} from '@angular/material/card'; 
 import { DragDropDirective } from './directives/drag-drop.directive';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -23,50 +23,53 @@ export class FileUploadComponent {
     return this.formGroup!;
   }
 
-  errorMsg = signal<string>('');
+  errorMsg = model<string>('');
   cErrorMsg = computed(() => this.errorMsg());
 
+  //max size in megabytes
+  maxSizeNumber = input<number>(5);
+  maxSize = computed(() => this.maxSizeNumber() * 1024 * 1024)
+  allowedTypes = input<string[]>([]);
+
+  formData = output<FormData>();
+
   onFileDropped(file: File | undefined) {
-    const allowedTypes = ['image/jpeg', 'image/png'];
-    const maxSize = 5 * 1024 * 1024; 
-
-    if (!allowedTypes.includes(file?.type as string)) {
-      this.fileUploadForm.get('file')?.setErrors({ invalidType: true });
-      this.errorMsg.set('Plik nie jest w odpowiednim formacie');
-      return;
+    const formData = new FormData();
+    if(file && this.validate(file)) {
+      formData.append('file', file);
+      formData.append('name', file.name);
+      this.formData.emit(formData);
     }
-
-    if (file?.size as number > maxSize) {
-      this.fileUploadForm.get('file')?.setErrors({ fileTooLarge: true });
-      this.errorMsg.set('Plik jest za duży');
-      return;
-    }
-
-    this.fileUploadForm.get('file')?.setErrors(null);
   }
 
   onFileClicked(event: Event) {
-    const input = event.target as HTMLInputElement
-
+    const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
-
-      const allowedTypes = ['image/jpeg', 'image/png'];
-      const maxSize = 5 * 1024 * 1024; 
-
-      if (!allowedTypes.includes(file?.type as string)) {
-        this.fileUploadForm.get('file')?.setErrors({ invalidType: true });
-        this.errorMsg.set('Plik nie jest w odpowiednim formacie');
-        return;
+      const formData = new FormData();
+      if(this.validate(file)) {
+        formData.append('file', file);
+        formData.append('name', file.name);
+        this.formData.emit(formData);
       }
-
-      if (file?.size as number > maxSize) {
-        this.fileUploadForm.get('file')?.setErrors({ fileTooLarge: true });
-        this.errorMsg.set('Plik jest za duży');
-        return;
-      }
-      this.fileUploadForm.get('file')?.setErrors(null);
-      this.errorMsg.set('');
     }
+  }
+
+  validate(file: File | undefined): boolean {
+    if (!this.allowedTypes().includes(file?.type as string)) {
+      this.fileUploadForm.get('file')?.setErrors({ invalidType: true });
+      this.errorMsg.set('Plik nie jest w odpowiednim formacie');
+      return false;
+    }
+
+    if (file?.size as number > this.maxSize()) {
+      this.fileUploadForm.get('file')?.setErrors({ fileTooLarge: true });
+      this.errorMsg.set('Plik jest za duży');
+      return false;
+    }
+    this.fileUploadForm.get('file')?.setErrors(null);
+    this.errorMsg.set('');
+
+    return true;
   }
 }
