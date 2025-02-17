5e118f05a6b04ed78f522329dd27933346264a4f
NAbrain <michal2002.60@gmail.com>
2025-02-17 19:51:59 +0100

    u

--- backend/src/main/java/com/scanner/service/fileUpload/FileUploadServiceException.java
+++ backend/src/main/java/com/scanner/service/fileUpload/FileUploadServiceException.java
@@ -3,8 +3,8 @@ package com.scanner.service.fileUpload;
 import com.scanner.exception.RestException;
 import org.springframework.http.HttpStatus;
 
-public class FileUploadException extends RestException {
-    public FileUploadException(String message, HttpStatus httpStatus) {
+public class FileUploadServiceException extends RestException {
+    public FileUploadServiceException(String message, HttpStatus httpStatus) {
         super(message, httpStatus);
     }
 }
