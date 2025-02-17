30c3e3f0f1bc542472f1a37f306fcf6a606d5c85
NAbrain <michal2002.60@gmail.com>
2025-02-17 19:51:49 +0100

    merge image processing stuff into a single method

--- backend/src/main/java/com/scanner/service/fileUpload/FileUploadService.java
+++ backend/src/main/java/com/scanner/service/fileUpload/FileUploadService.java
@@ -8,6 +8,7 @@ import com.scanner.service.ChatService;
 import com.scanner.service.ImageProcessor;
 import com.scanner.service.TesseractService;
 import net.sourceforge.tess4j.TesseractException;
+import org.springframework.ai.retry.NonTransientAiException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -33,18 +34,19 @@ public class FileUploadService {
     }
 
     public DocumentDto process(FileUploadRequestDto fileUploadRequest) {
-        BufferedImage image = readImage(fileUploadRequest);
-        BufferedImage resizedImage = imageProcessor.resizeImage(image, 3.3);
-        String ocrResult = performOCR(resizedImage);
-        String jsonContent = chatService.generateContent(ocrResult);
+        BufferedImage image = processImage(fileUploadRequest);
+        String ocrResult = performOCR(image);
+        String jsonContent = generateContent(ocrResult);
         return parseDocument(jsonContent);
     }
 
-    private BufferedImage readImage(FileUploadRequestDto request) {
+
+    private BufferedImage processImage(FileUploadRequestDto request) {
         try {
-            return ImageIO.read(request.file().getInputStream());
+            BufferedImage image = ImageIO.read(request.file().getInputStream());
+            return imageProcessor.resizeImage(image, 3.3);
         } catch (IOException e) {
-            throw new FileUploadException("Nie udało się przeczytać pliku.", HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new FileUploadServiceException("Nie udało się przeczytać pliku, spróbuj ponownie.", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -52,15 +54,27 @@ public class FileUploadService {
         try {
             return tesseractService.doOCR(image);
         } catch (TesseractException e) {
-            throw new FileUploadException("Nie udało się zeskanować pliku.", HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new FileUploadServiceException("Nie udało się przeprocesować pliku, spróbuj ponownie.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private String generateContent(String ocrResult) {
+        try {
+            return chatService.generateContent(ocrResult);
+        } catch (NonTransientAiException e) {
+            throw new FileUploadServiceException("Nie udało się wygenerować treści, spróbuj ponownie.", HttpStatus.BAD_REQUEST);
         }
     }
 
     private DocumentDto parseDocument(String json) {
         try {
+            System.out.println("output: " + json);
             return objectMapper.readValue(json, DocumentDto.class);
         } catch (JsonProcessingException e) {
-            throw new FileUploadException("Nie udało się przeprocesować pliku json.", HttpStatus.INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+            throw new FileUploadServiceException("Nie udało się wygenerować dokumentu, spróbuj ponownie.", HttpStatus.BAD_REQUEST);
         }
+
+
     }
 }
