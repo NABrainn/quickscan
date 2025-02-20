54d532d6a485a4070ee3c4e7273e4cc4ecaf26bb
NAbrain <michal2002.60@gmail.com>
2025-02-20 13:49:03 +0100

    implement: -actions for regenerating, saving documents -routing between steps

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,15 +2,29 @@ import { Routes } from '@angular/router';
 import { ScannerComponent } from './scanner/features/scanner/scanner.component';
 import { canActivateAuthRole } from './scanner/core/auth/auth-guard';
 import { PageNotFoundComponent } from './scanner/core/auth/components/page-not-found/page-not-found.component';
+import { FileUploadComponent } from './scanner/ui/file-upload/file-upload.component';
+import { DocumentCardComponent } from './scanner/ui/document-card/document-card.component';
+import { ReadyCardComponent } from './scanner/ui/ready-card/ready-card.component';
 
 export const routes: Routes = [
     { 
-        path: 'scanner', component: ScannerComponent,
-        canActivate: [canActivateAuthRole]
+        path: 'skaner', component: ScannerComponent,
+        canActivate: [canActivateAuthRole],
+        children: [
+            {
+                path: 'skanuj', component: FileUploadComponent
+            },
+            {
+                path: 'przeslij', component: DocumentCardComponent
+            },
+            {
+                path: 'gotowe', component: ReadyCardComponent
+            }
+        ]
     },
     {
         path: '',
-        redirectTo: 'scanner',
+        redirectTo: 'skaner',
         pathMatch: 'full',
         
     },
--- frontend/src/app/scanner/features/scanner/scanner.component.ts
+++ frontend/src/app/scanner/features/scanner/scanner.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input, OnInit, signal, TemplateRef, viewChild } from '@angular/core';
+import { Component, computed, inject, signal, viewChild } from '@angular/core';
 import {MatStepper, MatStepperModule} from '@angular/material/stepper'; 
 import {MatButtonModule} from '@angular/material/button';
 import { FileUploadComponent } from 'app/scanner/ui/file-upload/file-upload.component';
@@ -6,7 +6,9 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ScannerService } from './service/scanner.service';
 import { DocumentCardComponent } from 'app/scanner/ui/document-card/document-card.component';
 import { Invoice, Receipt } from 'app/scanner/shared/types';
-import { TestObjects } from 'app/scanner/shared/test-objects';
+import { ReadyCardComponent } from 'app/scanner/ui/ready-card/ready-card.component';
+import { Router } from '@angular/router';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-scanner',
@@ -14,23 +16,27 @@ import { TestObjects } from 'app/scanner/shared/test-objects';
     ReactiveFormsModule,
     MatStepperModule,
     MatButtonModule,
-    // FileUploadComponent,
+    FileUploadComponent,
     DocumentCardComponent,
+    ReadyCardComponent,
   ],
   templateUrl: './scanner.component.html',
   styleUrl: './scanner.component.css'
 })
 export class ScannerComponent{
 
-  test = inject(TestObjects);
   fb = inject(FormBuilder);
   service = inject(ScannerService);
+  router = inject(Router);
 
   _stepErrorMsg = signal<string>('');
   stepErrorMsg = computed(() => this._stepErrorMsg());
 
-  document = signal<Invoice | Receipt>(this.test.helperInvoice());
-  cDocument = computed(() => this.document());
+  private readonly _document = signal<Invoice | Receipt>({} as Invoice);
+  document = computed(() => this._document());
+
+  private _savedFormData = signal<FormData>(new FormData());
+  savedFormData = computed(() => this._savedFormData());
 
   stepper = viewChild(MatStepper);
   
@@ -39,16 +45,54 @@ export class ScannerComponent{
   })
 
   uploadFile(formData: FormData) {
+    this._savedFormData.set(formData);
     this.service.uploadFile(formData).subscribe({
       next: (res: Invoice | Receipt) => {
-        this.document.set(res);
+        this._document.set(res);
         this.stepper()?.next();
-        console.log(this.cDocument())
       },
       error: (res) => {
         this._stepErrorMsg.set(res?.error?.message);
         this.fileUploadForm?.reset();
       }
     });
+  }
+
+  regenerateDocument() {
+    this.service.uploadFile(this.savedFormData()).subscribe({
+      next: (res: Invoice | Receipt) => {
+        this._document.set(res);
+      },
+    });
+  }
+
+  saveDocument() {
+    this.service.saveDocument(this.document()).subscribe({
+      next: (res: Invoice | Receipt) => {
+        this.stepper()?.next();
+      },
+      error: (res) => console.log(res)
+    });
+  }
+
+  navigate(value: StepperSelectionEvent) {
+    switch(value.selectedIndex) {
+      case 0:
+        this.router.navigate(['skaner/skanuj']);
+        break;
+      case 1:
+        this.router.navigate(['skaner/przeslij']);
+        break;
+      case 2:
+        this.router.navigate(['skaner/gotowe']);
+        break;
+      default:
+        this.router.navigate(['skaner/skanuj']);
+    }
+  }
+
+  handleNavigate(value: string) {
+    this.router.navigate([value]);
+    this.stepper()?.reset();
   }
 }
