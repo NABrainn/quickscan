17f2a1a32ee06a2a18a6f0374947a45949ce652b
NAbrain <michal2002.60@gmail.com>
2025-03-03 12:43:20 +0100

    add page-not-found component to handle routes that dont exist

--- frontend/src/app/scanner/core/auth/components/page-not-found/page-not-found.component.ts
+++ frontend/src/app/scanner/core/auth/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-page-not-found',
     imports: [
         RouterLink,
-    
+        RouterLinkActive,
+        MatCard,
+        MatCardHeader,
+        MatIcon,
     ],
     templateUrl: './page-not-found.component.html',
     styleUrl: './page-not-found.component.css'
 })
 export class PageNotFoundComponent {
-
+    readonly router = inject(Router);
+    readonly url = this.router.url;
 }
