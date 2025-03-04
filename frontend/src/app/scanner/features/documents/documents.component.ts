f0600fc9e54585ee13b4b6557d2c0591ce190058
NAbrain <michal2002.60@gmail.com>
2025-03-04 12:53:05 +0100

    use service to fetch initial page with documents

--- frontend/src/app/scanner/features/documents/documents.component.ts
+++ frontend/src/app/scanner/features/documents/documents.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, Signal, signal } from '@angular/core';
+import { DocumentsService } from './services/documents.service';
+import {map} from 'rxjs';
+import { Document, Invoice, Receipt } from 'app/scanner/shared/types';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+enum Filter {
+  Invoice,
+  Receipt,
+  All
+}
 
 @Component({
   selector: 'app-documents',
-  imports: [],
+  imports: [
+  ],
   templateUrl: './documents.component.html'
 })
 export class DocumentsComponent {
+
+  private readonly service = inject(DocumentsService);
+
+  filterEnum = Filter;
+  private readonly _filterBy = signal<Filter>(Filter.Receipt);
+  readonly filterBy = computed(() => this._filterBy());
+  
+  documents: Signal<Document[] | undefined> = toSignal(this.service.getDocumentsPage().pipe(
+    map((page: any) => page.content as Document[]),
+  ));
+
+  readonly invoices = computed(() => this.documents()?.filter(doc => doc.type === 'invoice') as Invoice[]);
+  readonly receipts = computed(() => this.documents()?.filter(doc => doc.type === 'receipt') as Receipt[]);
 
 }
