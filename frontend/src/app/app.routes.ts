301ff9e554d1a57a89c8b45b7b52660c739f321c
NAbrain <michal2002.60@gmail.com>
2025-03-04 12:49:51 +0100

    add authguard to each route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { ScannerComponent } from './scanner/features/scanner/scanner.component';
-import { canActivateAuthRole } from './scanner/core/auth/auth-guard';
+import { authGuard } from './scanner/core/auth/auth-guard';
 import { stepperGuard } from './scanner/features/scanner/guards/stepper.guard';
 import { DocumentsComponent } from './scanner/features/documents/documents.component';
 
@@ -8,7 +8,7 @@ export const routes: Routes = [
     { 
         path: 'skaner', 
         component: ScannerComponent,
-        canActivate: [canActivateAuthRole],
+        canActivate: [authGuard],
         children: [
             {
                 path: '',
@@ -19,24 +19,24 @@ export const routes: Routes = [
             {
                 path: 'skanuj', 
                 loadComponent: () => import('./scanner/ui/file-upload/file-upload.component').then((c) => c.FileUploadComponent), 
-                canActivate: [stepperGuard]
+                canActivate: [authGuard, stepperGuard]
             },
             {
                 path: 'przeslij', 
                 loadComponent: () => import('./scanner/ui/document-card/document-card.component').then((c) => c.DocumentCardComponent), 
-                canActivate: [stepperGuard]
+                canActivate: [authGuard, stepperGuard]
             },
             {
                 path: 'gotowe', 
                 loadComponent: () => import('./scanner/ui/ready-card/ready-card.component').then((c) => c.ReadyCardComponent), 
-                canActivate: [stepperGuard]
+                canActivate: [authGuard, stepperGuard]
             }
         ]
     },
     {
         path: 'dokumenty',
         loadComponent: () => import('./scanner/features/documents/documents.component').then((c) => DocumentsComponent),
-        canActivate: [canActivateAuthRole],
+        canActivate: [authGuard],
     },
     {
         path: '',
