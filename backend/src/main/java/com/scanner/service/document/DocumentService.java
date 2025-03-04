140450e96b6c5dab71ad9d14a980d5bb59a56b64
NAbrain <michal2002.60@gmail.com>
2025-03-04 12:48:55 +0100

    properly map products to document

--- backend/src/main/java/com/scanner/service/document/DocumentService.java
+++ backend/src/main/java/com/scanner/service/document/DocumentService.java
@@ -1,25 +1,42 @@
 package com.scanner.service.document;
 
+import com.scanner.dto.document.DocumentDto;
+import com.scanner.dto.document.InvoiceDto;
+import com.scanner.dto.document.ReceiptDto;
 import com.scanner.entity.document.Document;
+import com.scanner.entity.document.Invoice;
+import com.scanner.entity.document.Receipt;
+import com.scanner.entity.product.ReceiptProduct;
+import com.scanner.repository.document.DocumentPagingAndSortingRepository;
 import com.scanner.repository.document.DocumentRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class DocumentService {
 
+    private final ModelMapper modelMapper;
     private final DocumentRepository documentRepository;
+    private final DocumentPagingAndSortingRepository documentPagingAndSortingRepository;
 
     public Document saveDocument(Document document) {
         try {
+            if (document instanceof Receipt receipt) {
+                Set<ReceiptProduct> products = receipt.getProducts();
+                if (products != null) {
+                    products.forEach(product -> product.setReceipt(receipt));
+                }
+            }
             return documentRepository.save(document);
-        }
-        catch (DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             throw new DocumentServiceException("Nip klienta/sprzedawcy istnieje w systemie.", HttpStatus.NOT_FOUND);
         }
     }
@@ -28,11 +45,19 @@ public class DocumentService {
         return documentRepository.findById(id).orElseThrow(() -> new DocumentServiceException("Document not found.", HttpStatus.NOT_FOUND));
     }
 
-    public Page<Document> getAllDocuments(String createdBy, Pageable pageable) {
-        return documentRepository.findByCreatedBy(createdBy, pageable);
+    public Page<DocumentDto> getDocumentPage(String createdBy, Pageable pageable) {
+        Page<Document> page = documentPagingAndSortingRepository.findByCreatedBy(createdBy, pageable);
+        return page.map(this::convertToDto);
     }
 
     public void deleteDocument(long id) {
         documentRepository.deleteById(id);
+    }
+    private DocumentDto convertToDto(Document document) {
+        if (document instanceof Invoice) {
+            return modelMapper.map(document, InvoiceDto.class);
+        } else if (document instanceof Receipt) {
+            return modelMapper.map(document, ReceiptDto.class);        }
+        throw new IllegalArgumentException("Unknown document type: " + document.getClass().getName());
     }
 }
