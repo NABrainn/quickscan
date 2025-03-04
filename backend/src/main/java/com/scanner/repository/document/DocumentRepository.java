57bc543868dc16c2ce8e02642dcb96e4e5d27d52
NAbrain <michal2002.60@gmail.com>
2025-03-04 12:49:23 +0100

    add Paging repository

--- backend/src/main/java/com/scanner/repository/document/DocumentRepository.java
+++ backend/src/main/java/com/scanner/repository/document/DocumentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DocumentRepository extends JpaRepository<Document, Long> {
-    Page<Document> findByCreatedBy(String createdBy, Pageable pageable);
+
 }
