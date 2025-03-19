087abd83f22e03d45de21e74e0d55c6e6ec3c16d
NAbrain <michal2002.60@gmail.com>
2025-03-19 16:18:16 +0100

    transition from openai api model to locally hosted model

--- backend/src/main/java/com/scanner/configuration/ChatClientConfiguration.java
+++ backend/src/main/java/com/scanner/configuration/ChatClientConfiguration.java
@@ -1,77 +1,32 @@
 package com.scanner.configuration;
 
-import org.springframework.ai.chat.client.ChatClient;
+import io.github.amithkoujalgi.ollama4j.core.OllamaAPI;
+import io.github.amithkoujalgi.ollama4j.core.exceptions.OllamaBaseException;
+import io.github.amithkoujalgi.ollama4j.core.models.OllamaResult;
+import io.github.amithkoujalgi.ollama4j.core.models.chat.OllamaChatMessageRole;
+import io.github.amithkoujalgi.ollama4j.core.models.chat.OllamaChatRequestBuilder;
+import io.github.amithkoujalgi.ollama4j.core.models.chat.OllamaChatRequestModel;
+import io.github.amithkoujalgi.ollama4j.core.models.chat.OllamaChatResult;
+import io.github.amithkoujalgi.ollama4j.core.types.OllamaModelType;
+
+import io.github.amithkoujalgi.ollama4j.core.utils.OptionsBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.util.List;
 
 @Configuration
 public class ChatClientConfiguration {
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.defaultSystem("""
-                        Sprawdź jaki rodzaj dokumentu został przesłany: paragon lub faktura, a następnie:
-                        
-                        jeżeli rodzaj dokumantu to faktura:
-                        przeczytaj zdjęcie faktury i uzyskaj z niego nastepujące informacje. 
-                        Całość napisz w podanym formacie json zmieniając tylko value, key muszą zostać nie zmienione. 
-                        Napisz tylko json w podanym niżej formacie. 
-                        Jeśli jest informacja w nawiasie, zastosuj się do tej informacji, nie wstawiając jej w odpowiedź. 
-                        Jeśli nie udało się znaleźć informacji, napisz 'null'. 
-                        Dane MUSZĄ mieć następujące nazwy:
-                        {
-                            "type": "invoice" (nie zczytuj z faktury, po prostu wpisz "invoice")
-                            "odbiorca":{
-                                "nazwa": "nazwa",
-                                "nip": "nip",
-                                "adres":"adres"
-                            },
-                            "sprzedawca":{ 
-                                "nazwa":"nazwa", 
-                                "nip":"nip", 
-                                "adres":"adres"
-                            },
-                            "numerFaktury": "numerFaktury",  
-                            "nrRachunkuBankowego": "nrRachunkuBankowego", 
-                            "dataWystawienia": "dataWystawienia",  
-                            "dataSprzedaży": "dataSprzedazy",  
-                            "razemNetto": "razemNetto",  
-                            "razemStawka": "razemStawka",  
-                            "razemPodatek": "razemPodatek",  
-                            "razemBrutto": "razemBrutto", 
-                            "waluta": "waluta",  
-                            "formaPłatności": "formaPlatnosci",   
-                            "produkty": [   (jeśli tablica jest pusta, wpisz "[]")
-                                    {     
-                                        "nazwaProduktu": "nazwaProduktu",    
-                                        "jednostkaMiary": "jednostkaMiary" (zobacz czy nie istnieje skrót 'j. m.' zapisz wartość jako value jednostkiMiary. key jednostkaMiary bez zmian),      
-                                        "ilość":  "ilość"(jeśli ilość nie jest integerem napisz tylko float np. 0.55, zawsze w String),  
-                                        "wartośćNetto": "wartoscNetto",  
-                                        "stawkaVAT": "stawkaVat",  
-                                        "podatekVAT": "podatekVat",  
-                                        "wartośćBrutto": "brutto" 
-                                    }  
-                            ]
-                        }
+    public OllamaAPI ollamaAPIConfig() throws OllamaBaseException, IOException, URISyntaxException, InterruptedException {
+        OllamaAPI ollamaAPI = new OllamaAPI("http://localhost:11434");
+        ollamaAPI.setRequestTimeoutSeconds(20);
+        ollamaAPI.pullModel(OllamaModelType.LLAVA);
+        return ollamaAPI;
 
-                        jeżeli rodzaj dokumentu to paragon:
-                        przeczytaj zdjęcie paragonu i uzyskaj z niego nastepujące informacje. 
-                        Całość napisz w podanym formacie json zmieniając tylko value, key muszą zostać nie zmienione. 
-                        Napisz tylko json w podanym niżej formacie. Jeśli jest informacja w nawiasie, zastosuj się do tej informacji, nie wstawiając jej w odpowiedź. 
-                        Jeśli nie udało się znaleźć informacji, napisz 'null'. Dane MUSZĄ mieć następujące nazwy:
-                        { 
-                            "type": "receipt", (nie zczytuj z paragonu, po prostu wpisz "receipt")
-                            "dataZakupu": "dataZakupu",  
-                            "nazwaSklepu": "nazwaSklepu", 
-                            "kwotaCałkowita": "kwotaCałkowita",  
-                            "produkty": [    
-                                {     
-                                    "nazwaProduktu": "nazwaProduktu",    
-                                    "cenaSuma": "cenaSuma",      
-                                    "ilość":  "ilość"(jeśli ilość nie jest integerem napisz tylko float np. 0.55)    
-                                }  
-                            ]
-                        }
-                        """)
-                .build();
     }
 }
--- backend/src/main/java/com/scanner/service/scanner/fileUploadService/FileUploadService.java
+++ backend/src/main/java/com/scanner/service/scanner/fileUploadService/FileUploadService.java
@@ -5,57 +5,39 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scanner.dto.document.DocumentDto;
 import com.scanner.dto.FileUploadRequestDto;
 import com.scanner.service.chatService.ChatService;
-import com.scanner.service.chatService.ContentType;
+import com.scanner.utility.StringUtils;
+import io.github.amithkoujalgi.ollama4j.core.exceptions.OllamaBaseException;
 import lombok.RequiredArgsConstructor;
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
-import org.springframework.ai.retry.NonTransientAiException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 @RequiredArgsConstructor
 @Service
 public class FileUploadService {
 
-    private final ImageProcessor imageProcessor;
-    private final Tesseract tesseract;
     private final ChatService chatService;
     private final ObjectMapper objectMapper;
 
     public DocumentDto process(FileUploadRequestDto fileUploadRequest) {
-        BufferedImage image = processImage(fileUploadRequest);
-        String ocrResult = performOCR(image);
-        String jsonContent = generateContent(ocrResult);
-        return parseDocument(jsonContent);
+        String jsonContent = generateContent(fileUploadRequest);
+        return parseDocument(StringUtils.jsonFromText(jsonContent));
     }
 
-    private BufferedImage processImage(FileUploadRequestDto request) {
+    private String generateContent(FileUploadRequestDto request) {
         try {
-            BufferedImage image = ImageIO.read(request.file().getInputStream());
-            imageProcessor.deskew(image);
-            return image;
-        } catch (IOException e) {
-            throw new FileUploadServiceException("Nie udało się przeczytać pliku, spróbuj ponownie.", HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    private String performOCR(BufferedImage image) {
-        try {
-            return tesseract.doOCR(image);
-        } catch (TesseractException e) {
-            throw new FileUploadServiceException("Nie udało się przeprocesować pliku, spróbuj ponownie.", HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    private String generateContent(String ocrResult) {
-        try {
-            System.out.println(ocrResult);
-            return chatService.generateContent(ocrResult, ContentType.JSON);
-        } catch (NonTransientAiException e) {
+            var multipartFileName = request.file().getOriginalFilename();
+            String extension = "";
+            if (multipartFileName != null && multipartFileName.contains(".")) {
+                extension = multipartFileName.substring(multipartFileName.lastIndexOf("."));
+            }
+            File file = File.createTempFile("image", extension);
+            request.file().transferTo(file);
+            return chatService.generateContent(file);
+        } catch (RuntimeException | IOException | OllamaBaseException | InterruptedException e) {
             throw new FileUploadServiceException("Nie udało się wygenerować treści, spróbuj ponownie.", HttpStatus.BAD_REQUEST);
         }
     }
